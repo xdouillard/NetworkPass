6fc1a40a74bed0b585fcb71c3ae36ab4fe912d4c
Xavier <xavier.douillard@gmail.com>
2019-03-26 22:57:29 +0100

    Validation pass_linux

--- pass_linux_change.py
+++ pass_linux_change.py
@@ -3,26 +3,28 @@
 import paramiko
 import time
 
-ip_address = "192.168.1.1" 
-username = "xavier"
-password = "test1234"
-root_password = "root1234"
-new_pw = "admin1234"
+ip_address = "192.168.0.254" 
+username = "admin"
+password = "admin1234"
+password_root = "root1234"
+new_pw = "test1234"
 
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy() )
 ssh_client.connect(hostname=ip_address,username=username,password=password)
 
-print ("Successfuly connected to"),  ip_address
+print ("Successfuly connected to",  ip_address)
 
 remote_connection = ssh_client.invoke_shell()
-
 remote_connection.send("su\n")
-remote_connection.send((root_password) + "\n")
-remote_connection.send("passwd" + (username) + "\n")
-remote_connection.send((new_pw) + "\n")
-remote_connection.send((new_pw) + "\n")
+time.sleep(2)
+remote_connection.send((password_root) + "\n")
+time.sleep(2)
+remote_connection.send("echo " + (username) + ":" + (new_pw) + " | chpasswd\n")
+time.sleep(2)
+remote_connection.send("exit\n")
 
-time.sleep(1)
+output = remote_connection.recv(65535)
+print (output)
 
 ssh_client.close
