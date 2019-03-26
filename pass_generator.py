d3289b74d5838d8a13dceca6f8c660cfaf8f4aff
Xavier Douillard <xavier.douillard@gmail.com>
2019-03-26 10:02:45 +0100

    Validation de pass_generator.py

--- pass_generator.py
+++ pass_generator.py
@@ -2,36 +2,38 @@
 import sys
 import secrets
 
-#Defines all possible characters for the password
+#Définition des caractères utilisables pour le mots de passe
 lowercase = "abcdefghijklmnopqrstuvwxyz"
 uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 nums = "1234567890"
 special_char = "!@#$%^&*()=+-~/[]{};:<>`"
 
-#Changes input to an integer value
+#Variables du mot de passe
+password = ''
+count = 0
 length = 12
 upper_req = 4
 lower_req = 4
 num_req = 4
 special_req = 4
 
-#Determines if there was an error. If there wasn't, then the password is generated
+#Génération du password
 while (count < length):
-	if (upper_req > 0) and (count < length):
-            password += secrets.choice(uppercase)
-            count += 1
+    if (upper_req > 0) and (count < length):
+        password += secrets.choice(uppercase)
+        count += 1
 
-	if (lower_req > 0) and (count < length):
-            password += secrets.choice(lowercase)
-            count += 1
+    if (lower_req > 0) and (count < length):
+        password += secrets.choice(lowercase)
+        count += 1
 
-        if (num_req > 0) and (count < length):
-            password += secrets.choice(nums)
-            count += 1
+    if (num_req > 0) and (count < length):
+        password += secrets.choice(nums)
+        count += 1
 
-        if (special_req > 0) and (count < length):
-            password += secrets.choice(special_char)
-            count += 1
+    if (special_req > 0) and (count < length):
+        password += secrets.choice(special_char)
+        count += 1
 
-#Displays the password
+#Edition du password
 print ("PASSWORD: %s" % password)
